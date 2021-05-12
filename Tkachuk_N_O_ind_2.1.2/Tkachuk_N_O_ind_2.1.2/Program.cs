71c4d76cde45508d154af8c300418500f2a1a93d
Natalia Tkachuk <tkachuk_n@donnu.edu.ua>
2021-05-12 23:16:56 +0300

    Tkachuk_N_O_ind_2.1.2

---
+++ Tkachuk_N_O_ind_2.1.2/Tkachuk_N_O_ind_2.1.2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+
+namespace ind_2_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.WriteLine("Кiлькiсть елементiв масиву: ");
+            n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+
+            Console.WriteLine("Введiть чиcла масиву: ");
+            input_array(array, n);
+
+            
+
+            Console.WriteLine("Початковий масив: ");
+
+         
+            output_array(array, n);
+
+
+            Console.WriteLine("\nВiдсортований масив: ");
+
+            bubble_sort(array, n);
+            output_array(array, n);
+
+            Console.Read();
+        }
+
+
+        static void input_array(int[] array, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("array[" + i + "] = ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        static void output_array(int[] array, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(array[i] + "\t");            
+            }
+        }
+
+        static void bubble_sort(int[] array, int n)
+        {
+            int a;
+            bool temp = true;
+
+            while (temp)
+            {
+                temp = false;
+
+                for (int i = 1; i < n; i++)
+                {
+                    if (array[i - 1] < array[i])
+                    {
+                        a = array[i - 1];
+                        array[i - 1] = array[i];
+                        array[i] = a;
+                        
+                        temp = true;
+                    }
+                }
+
+            }
+
+        }
+
+    }
+
+}
